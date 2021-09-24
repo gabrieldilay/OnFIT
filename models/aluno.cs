b4f48d300b611926edea24a25c232c324bec17f9
Walups <74693883+Walups@users.noreply.github.com>
2021-09-24 19:52:41 -0300

    Update aluno.cs

--- models/aluno.cs
+++ models/aluno.cs
@@ -8,6 +8,6 @@ public class Aluno : Usuario{
     public int Idade { get; set; }
     public double Altura { get; set; }
     public double Peso { get; set; }
-    //public string Objetivo { get; set; }
+    public string Objetivo { get; set; }
     }
 }
