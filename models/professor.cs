8f1b8520cf10389ee922cca8877d1682f7a51dee
Walups <74693883+Walups@users.noreply.github.com>
2021-09-24 19:53:13 -0300

    Update professor.cs

--- models/professor.cs
+++ models/professor.cs
@@ -5,6 +5,6 @@ public class Professor : Usuario
     public  string Nome { get; set; }
     public  string  Email { get; set; }
     public  int  Idade { get; set; }
-    public  double  Cref { get; set; }
+    //public  double  Cref { get; set; }
 }
 }
